cc3f94e2783a744fc76798482f8b4d5bf9a0fbfd
yechen <yechen@acm.org>
2017-07-17 16:51:51 -0700

    challenges

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
 	url(r'^polls/', include('polls.urls')),
+	url(r'^challenges/', include('challenges.urls')),
 	url(r'^admin/', admin.site.urls),
 ]
