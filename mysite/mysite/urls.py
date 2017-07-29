9a70d6a99ae77f22d06412f04f10c3bd750ffe43
yechen <yechen@acm.org>
2017-07-29 08:37:14 -0700

    login

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
 	url(r'^polls/', include('polls.urls')),
 	url(r'^challenges/', include('challenges.urls')),
 	url(r'^admin/', admin.site.urls),
+	url(r'^login/$', auth_views.login, name='login'),
+	url(r'^logout/$', auth_views.logout, name='logout'),
 	url(r'^$', views.index, name='index'),
 ]
